242e708ba1d071283804aaffa5060d0637fad491
Nafisah Nubah <Nafisah.Nubah@dal.ca>
2024-12-08 22:18:51 -0400

    minor change

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,23 +5,15 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA8nwUsIlOEZmdFWznclL__iePd3xp4XtA",
-  authDomain: "cinemate-ef377.firebaseapp.com",
-  projectId: "cinemate-ef377",
-  storageBucket: "cinemate-ef377.firebasestorage.app",
-  messagingSenderId: "813475206432",
-  appId: "1:813475206432:web:27343e17368d493c3cf345"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
